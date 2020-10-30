d7fdde7ae6dbe70efe6fb663cc0dda25ad50a0d3
shivafobl <shivasaifobl@gmail.com>
2020-10-30 18:41:44 +0530

    df

--- usermanagement/settings.py
+++ usermanagement/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'm9sj5+2()zo4ih^o!@9w^(_o+njts!7m3wj+i$z0#7n)_0_#kr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
